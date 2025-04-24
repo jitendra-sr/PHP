fac85d66e185b0750fafefb009631cd2b1336e71
jitendra-sr <singh.jitendra.up85@gmail.com>
2025-04-24 13:24:02 +0530

    Session and its security

---
+++ first-project/config.php
@@ -0,0 +1,24 @@
+<?php
+
+// php.ini settings for session security
+ini_set('session.use_only_cookies', 1); // Prevent session hijacking by using only cookies for session ID
+ini_set('session.use_strict_mode', 1); // Prevent session fixation by using strict mode for session IDs
+
+session_set_cookie_params([
+    'lifetime' => 1800, // Cookie expires after 30 minutes
+    'path' => '/', // Cookie is available within the entire domain
+    'domain' => $_SERVER['HTTP_HOST'], // Cookie is available for the current domain
+    'secure' => true, // Cookie is only sent over secure HTTPS connections
+    'httponly' => true, // Cookie is not accessible via JavaScript (helps prevent XSS attacks)
+]);
+session_start(); // Start the session
+
+if(!isset($_SESSION['last_regeneration'])) {
+    session_regenerate_id(true); // Regenerate the session ID for the first time
+    $_SESSION['last_regeneration'] = time(); // Set the last regeneration time
+} else {
+    if(time() - $_SESSION['last_regeneration'] >= 1800) { // If more than 30 minutes have passed since the last regeneration
+        session_regenerate_id(true); // Regenerate the session ID
+        $_SESSION['last_regeneration'] = time(); // Update the last regeneration time
+    }
+}
---
+++ first-project/session.php
@@ -0,0 +1,30 @@
+<?php
+session_start(); // Start the session
+$_SESSION["username"] = "JohnDoe"; // Set a session variable
+
+// Note: No matter that the session variable is set in this file, it will be available in all files that have session_start() at the beginning of the file. This is because the session data is stored on the server and is accessible across different pages as long as the session is active.
+
+// unset($_SESSION["username"]); // Unset the session variable
+// session_unset(); // Unset all session variables
+
+session_destroy(); // Destroy the session and all session variables
+// It is important to note that session_destroy() does not unset the $_SESSION variable. It only destroys the session data on the server. The $_SESSION variable will still exist on the page it started, but it will be empty to other pages. To unset the $_SESSION variable regardless of page, you can use unset($_SESSION) or session_unset().
+
+require_once 'config.php'; // Session with security settings
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Session started</h1>";
+    echo "<p>Username: " . $_SESSION["username"] . "</p>"; // Access the session variable
+    ?>
+</body>
+</html>
